dd8a631284c38fcd904f2e694cc5acb13271d6e5
Daniel Harasim <daniel.harasim@gmx.de>
2018-08-21 15:46:57 +0200

    rm promotion_rule, convert, and exp methods, add log and float methods

--- src/LogProbs.jl
+++ src/LogProbs.jl
@@ -1,6 +1,8 @@
+__precompile__()
+
 module LogProbs
 
-import Base: show, promote_rule, convert, exp, rand, isapprox
+import Base: show, log, float, rand, isapprox
 import Base: ==, hash
 import Base: zero, one, +, -, *, /, isless
 
@@ -9,30 +11,30 @@ using StatsFuns: logaddexp, log1mexp
 export LogProb, information
 
 struct LogProb <: Number
-    value :: Float64
+    log :: Float64
     LogProb(number; islog::Bool=false) = islog ? new(number) : new(log(number))
 end
 
-==(x::LogProb, y::LogProb)       = x.value == y.value
-hash(x::LogProb, y::LogProb)     = hash(LogProb, hash(x.value))
-rand(::Type{LogProb})            = LogProb(rand())
-isapprox(x::LogProb, y::LogProb) = isapprox(x.value, y.value)
-show(io::IO, x::LogProb)         = print(io, "LogProb($(exp(x)))")
-exp(x::LogProb)                  = exp(x.value)
-information(x::LogProb)          = - x.value / log(2)
+show(io::IO, x::LogProb)  = print(io, "LogProb($(float(x)))")
 
-one(::Type{LogProb})      = LogProb(1)
+==(x::LogProb,y::LogProb) = x.log == y.log
+hash(x::LogProb)          = hash(LogProb, hash(x.log))
+
+rand(::Type{LogProb})     = LogProb(rand())
+
+float(x::LogProb)         = exp(x.log)
+log(x::LogProb)           = x.log
+information(x::LogProb)   = - log(x) / log(2)
+
+one( ::Type{LogProb})     = LogProb(1)
 zero(::Type{LogProb})     = LogProb(0)
 
-*(x::LogProb, y::LogProb) = LogProb(x.value + y.value, islog=true)
-/(x::LogProb, y::LogProb) = LogProb(x.value - y.value, islog=true)
-+(x::LogProb, y::LogProb) = LogProb(logaddexp(x.value, y.value), islog=true)
--(x::LogProb, y::LogProb) = LogProb(x.value + log1mexp(y.value-x.value), islog=true)
+*(x::LogProb, y::LogProb) = LogProb(x.log + y.log, islog=true)
+/(x::LogProb, y::LogProb) = LogProb(x.log - y.log, islog=true)
++(x::LogProb, y::LogProb) = LogProb(logaddexp(x.log, y.log), islog=true)
+-(x::LogProb, y::LogProb) = LogProb(x.log + log1mexp(y.log-x.log), islog=true)
 
-isless(x::LogProb, y::LogProb) = isless(x.value, y.value)
-
-promote_rule(::Type{LogProb}, ::Type{Int})     = LogProb
-promote_rule(::Type{LogProb}, ::Type{Float64}) = LogProb
-convert(::Type{LogProb}, x::Real)              = LogProb(x)
+isless(  x::LogProb, y::LogProb) = isless(  x.log, y.log)
+isapprox(x::LogProb, y::LogProb) = isapprox(x.log, y.log)
 
 end # Module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,17 +1,31 @@
 using LogProbs
 using Test
 
-p = LogProb(.2)
-q = LogProb(.5)
-r = LogProb(.1)
+p = LogProb(0.2)
+q = LogProb(0.5)
+r = LogProb(0.1)
 
 @test p * q ≈ r
-@test exp(p+q) ≈ 0.7
+@test float(p+q) ≈ 0.7
+@test log(p) == -1.6094379124341003
 @test information(q) ≈ 1
-@test r * 2 ≈ p
+@test r * LogProb(2) ≈ p
 @test q - p ≈ LogProb(.3)
 @test p / q ≈ LogProb(.4)
 @test p + zero(LogProb) ≈ p
 @test p *  one(LogProb) ≈ p
 @test p < q
 @test LogProb(0) < p
+
+@test one(LogProb).log == 0
+@test zero(LogProb).log == -Inf
+
+@test string(p) == "LogProb(0.2)"
+
+@test let d = Dict(LogProb(x) => x for x in [0.2, 0.5, 0.1])
+    (d[p], d[q], d[r]) == (0.2, 0.5, 0.1)
+end
+
+@test let s = rand(LogProb)
+    s.log == log(s)
+end
